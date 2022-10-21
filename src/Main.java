b84c8e8a79a64ac198a9dc2852619eb16857b29e
Acer <atabekovaayona@gmail.com>
2022-10-21 18:53:32 +0300

    #StandUp  3 урок  *Что сделал: Пересмотрела стрим , посмотрела дополнительные видео прочитала информации по сортированию массива, сделала дз.  *Проблемы:нет .  *Что буду делать: Приступать к 4 дз

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        double[] numbers = {3.0, 5.2, 11.0, -7.3, 14.9, -1.0, -23.4, 6.5, -10.9, -33.1, 14.0, 15.0, -2.1, -9.7, 18.8};
-        //13,84 среднее арифметическое число всех положительных, после первого отрицательного
-        double num1 = 13.84;
+        
+        double[] numbers = {18.8, -9.7, -2.1, 14.0, 15.0, -33.1, -10.9, 6.5, -23.4, -1.0, 14.9, -7.3, 11.0, 5.2, 3.0,};
+        boolean numbers2 = false;
+        double num1  = 0 ;
+        int num2 = 0;
 
 
-        for (double i = 67; i > 5; i++) {
-            System.out.println("Среднее арифметическое число " + num1);
-            break;
 
+        for ( double num: numbers ) {
+            if (num < 0) {
+                numbers2 = true;
+            }
+            if (numbers2) {
+                if (num > 0) {
+                    num1 = num1 + num;
+                    num2++;
+
+
+                }
+            }
         }
+           double num3 = num1 / num2;
+            System.out.println(num3 );
+
     }
 }
